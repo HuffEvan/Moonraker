22f628a96571f6c6a1d1175dd6892aa1875bfba6
Evan Huff <turtle8060@gmail.com>
2019-01-26 15:07:48 -0500

    finished coed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,13 +2,20 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import frc.robot.controllers.Wingman;
+import frc.robot.controllers.Xbox;
+import frc.robot.subsytem.Arm;
 import frc.robot.subsytem.Chassis;
+import frc.robot.subsytem.Elevator;
 
 
 public class Robot extends TimedRobot {
 
   private static final Chassis chassis = new Chassis(); 
   private static final Wingman driver = new Wingman();
+  private static final Elevator elevator = new Elevator();
+  private static final Xbox coDriver = new Xbox();
+  private static final Arm arm = new Arm(); 
+  private static final Elevator lift = new Elevator();
 
   @Override
   public void robotInit() {
@@ -26,8 +33,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousPeriodic() {
-    chassis.left(0.3);
-    chassis.right(0.3);
+    elevator.lift(driver.getY());
   }
 
 
@@ -35,10 +41,27 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     chassis.left(driver.getY());
     chassis.right(driver.getY());
+
+    if (driver.getThumb() == true) {
+      arm.open();
+    } else {
+      arm.closed();   
+    }
+  
+  
+    if (coDriver.getRB() == true) {
+      lift.up();
+    } else {
+      lift.down();
+    }
+    
   }
+  
 
   
   @Override
   public void testPeriodic() {
   }
 }
+
+
