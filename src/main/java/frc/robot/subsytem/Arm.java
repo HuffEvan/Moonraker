cd1d67f97321680fdd5df8e23d7ec35efe9c38c6
Joshua Winters-Brown <bornwbear1002@gmail.com>
2019-02-16 11:49:10 -0500

    Added functional arm abbilitys

--- src/main/java/frc/robot/subsytem/Arm.java
+++ src/main/java/frc/robot/subsytem/Arm.java
@@ -1,28 +1,30 @@
 package frc.robot.subsytem;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import frc.robot.Map;
 
 public class Arm{
-    private static DoubleSolenoid actuator;
+    private static Solenoid actuator;
+    private static Solenoid actuator1;
     private static Spark axis;
 
     public Arm () {
-        actuator = new DoubleSolenoid(0, 1);
+        actuator = new Solenoid(6);
+        actuator1 = new Solenoid(1);
+
         axis = new Spark(5);
     }
 
     public void open (){
 
-        actuator.set(DoubleSolenoid.Value.kOff);
+        actuator.set(true);
+        actuator1.set(false);
 
     } 
     public void closed (){
 
-        actuator.set(DoubleSolenoid.Value.kReverse);
+        actuator.set(false);
+        actuator1.set(true);
     }
 
    
